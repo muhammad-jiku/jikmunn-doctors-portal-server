0a0a710d86b719d14ba229f8a387ef57d0c8bb51
muhammad-jiku <muhammadjiku364@gmail.com>
2022-05-18 13:22:37 +0600

    mongodb added and api added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,6 +14,34 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello there!');
 });
+
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASSWORD}@cluster0.w6ky9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db('doctorsPortal')
+      .collection('services');
+
+    // displaying services
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    // await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
